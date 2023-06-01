bfd69e5ee323d9c0772f41e2fca510f1c128e37a
Spike Curtis <spike@coder.com>
2023-06-01 08:03:46 +0000

    Fix symlinking
    Signed-off-by: Spike Curtis <spike@coder.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-ln -s -T -f ~/ .bash_aliases .bash_logout .bashrc .profile
+wd=`pwd`
+ln -s -f $wd/.bash_aliases $wd/.bash_logout $wd/.bashrc $wd/.profile ~/
 
 sudo apt-get update
 sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
