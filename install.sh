f1ee73de1911038a388a6c705f57954f6c80d278
Spike Curtis <spike@coder.com>
2023-06-23 07:06:59 +0000

    add git config
    Signed-off-by: Spike Curtis <spike@coder.com>

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s -f $wd/.bash_aliases $wd/.bash_logout $wd/.bashrc $wd/.profile ~/
 
 sudo apt-get update
 sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
+git config --global user.email spike@coder.com
+git config --global user.name "Spike Curtis"
+
