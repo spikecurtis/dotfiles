dd7bb41f64204d1236ef3b942ca9ff7c35335de1
Spike Curtis <spike@coder.com>
2023-10-18 14:46:31 +0400

    Add graphite

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 wd=`pwd`
 ln -s -f $wd/.bash_aliases $wd/.bash_logout $wd/.bashrc $wd/.profile ~/
 
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin git
+sudo npm i -g @withgraphite/graphite-cli
 git config --global user.email spike@coder.com
 git config --global user.name "Spike Curtis"
-
