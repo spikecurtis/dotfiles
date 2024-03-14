e537107daa978bf667559a20133559713a3fe688
Spike Curtis <spike@coder.com>
2024-03-14 15:23:59 +0400

    remove git PPA

--- install.sh
+++ install.sh
@@ -3,10 +3,8 @@
 wd=`pwd`
 ln -s -f $wd/.bash_aliases $wd/.bash_logout $wd/.bashrc $wd/.profile ~/
 
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin git
+sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
 sudo npm i -g @withgraphite/graphite-cli
 git config --global user.email spike@coder.com
 git config --global user.name "Spike Curtis"
