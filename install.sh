9de2a7d7098d0b1cd4dbd2854f4b1c751c67c2f8
Spike Curtis <spike@coder.com>
2024-05-31 09:40:14 +0400

    Update install.sh
    DPkg lock timeout

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 wd=`pwd`
 ln -s -f $wd/.bash_aliases $wd/.bash_logout $wd/.bashrc $wd/.profile ~/
 
-sudo apt-get update
-sudo apt-get install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt-get -o DPkg::Lock::Timeout=90 update
+sudo apt-get -o DPkg::Lock::Timeout=90 install -y nano tmux google-cloud-sdk-gke-gcloud-auth-plugin
 sudo npm i -g @withgraphite/graphite-cli
 git config --global user.email spike@coder.com
 git config --global user.name "Spike Curtis"
